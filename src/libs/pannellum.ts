ed3a1ee3c081d0f4ff410974457cb4c4356cfc07
Luan Himmlisch <luan@himmlisch.com.mx>
2026-04-26 20:14:17 -0600

    use local pannellum library

--- src/libs/pannellum.ts
+++ src/libs/pannellum.ts
@@ -1,3 +1,3 @@
-import 'pannellum/src/js/libpannellum.js';
-import 'pannellum/src/js/pannellum.js';
-import 'pannellum/src/css/pannellum.css';
+import './pannellum/libpannellum.js';
+import './pannellum/pannellum.js';
+import './pannellum/pannellum.css';
